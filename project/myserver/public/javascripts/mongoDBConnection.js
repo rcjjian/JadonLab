eda983750a827405349d5e41a8406cb4d781efd3
JadonYuen <rcjjian@163.com>
2017-05-24 16:45:31 +0800

    更新mongodb操作和更新express笔记md格式

--- project/myserver/public/javascripts/mongoDBConnection.js
+++ project/myserver/public/javascripts/mongoDBConnection.js
@@ -1,25 +1,46 @@
 var mongoose = require('mongoose');
 
-class mongooseConnection{
+var _instance;
 
-	 
+var _modelMap;
+
+class MongooseConnection{
+
 	constructor() {
+		if(_instance == null){
+			_instance = this;
+			this.registerModel();
+		}
+		return _instance;
+	}
+
+	registerModel() {
+		this.setModel('user',{username:String,password:String});
 	}
 
 	getConnection() {
-
-
-		if(this.dbConnection == null)
-			this.dbConnection = mongoose.connect('mongodb://localhost:27017/myDB');
-		return this.dbConnection;
+		return mongoose.connect('mongodb://localhost:27017/myDB');
 	}
 
-	getModel(collectionName,obj) {
-		var Schema = mongoose.Schema;
-		var schema = new Schema(obj);
-		return this.getConnection().model(collectionName,schema);
+	setModel(collectionName,obj) {
+		if(_modelMap == null)
+			_modelMap = {};
+
+		if(_modelMap[collectionName] == null){
+			var Schema = mongoose.Schema;
+			var schema = new Schema(obj);
+			_modelMap[collectionName] = this.getConnection().model(collectionName,schema);
+		}
+
+	}
+
+	getModel(collectionName) {
+
+		if(!_modelMap)return null;
+
+		else return _modelMap[collectionName];
 	}
 
 }
 
-module.exports = mongooseConnection;
+module.exports = MongooseConnection;
--- project/myserver/routes/login.js
+++ project/myserver/routes/login.js
@@ -6,39 +6,49 @@ var router = express.Router();
 var session = require('express-session');
 
 var MongoDBConnect = require('../public/javascripts/mongoDBConnection');
+var mongoDB = new MongoDBConnect();
 
 router.post('/',function(req,res,next) {
 
     var name = req.body.username;
     var pwd = req.body.password;
 
-    var myMongoDBConnect = new MongoDBConnect();
-    var userModle = myMongoDBConnect.getModel("user",{username:String,password:String});
+    var userModel = mongoDB.getModel("user");
     var query = {
         username : name,
         password : pwd
     };
-    userModle.count(function(err,doc) {
-        console.log(err + "~~~~");
-        if(!err){
-            if(doc == 1){
-                session.user = {
-                    username : name,
-                    password : pwd
-                };
-                return res.send({  //返回json格式数据
-                    status : 1,
-                    msg : '登录成功',
-                    user : session.user
-                });
-            }else{
-                return res.send({
-                    status : 0,
-                    msg : '登录失败'
-                });
-            }
-        }
-    },query);
+
+    new userModel(query).save(function(err) {
+        console.log(err);
+    });
+
+
+    userModel.find(function(err,doc) {
+        console.log(doc);
+    });
+
+    //userModel.count(query,function(err,doc) {
+    //    console.log(doc);
+    //    if(!err){
+    //        if(doc == 1){
+    //            session.user = {
+    //                username : name,
+    //                password : pwd
+    //            };
+    //            return res.send({  //返回json格式数据
+    //                status : 1,
+    //                msg : '登录成功',
+    //                user : session.user
+    //            });
+    //        }else{
+    //            return res.send({
+    //                status : 0,
+    //                msg : '登录失败'
+    //            });
+    //        }
+    //    }
+    //});
 
 });
 
